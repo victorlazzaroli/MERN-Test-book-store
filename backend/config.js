1d5726823cc19c308928c45eb1a6f6df884cbae8
Victor <the_wizard_ita@hotmail.it>
2023-09-01 10:17:21 +0200

    Mern test app

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  '';
---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+
+const ShowBook = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Dettagli</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Id</span>
+            <span>{book._id}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Titolo</span>
+            <span>{book.title}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Autore</span>
+            <span>{book.author}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Anno</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Creato il</span>
+            <span>{new Date(book.createdAt).toString()}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Ultima modifica</span>
+            <span>{new Date(book.updatedAt).toString()}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShowBook;
